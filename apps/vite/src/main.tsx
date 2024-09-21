ac8f549d0c36e7f443f68bd0e8ec920967c331aa
Sonny Recio <reciosonny@gmail.com>
2024-09-21 19:15:40 +0800

    Added sample code in vite for rendering different entry point elements.

--- apps/vite/src/main.tsx
+++ apps/vite/src/main.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+// DOM element
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
 
-ReactDOM.createRoot(document.getElementById("containerElement")!).render(<h1 className="text-2xl">Hello world! this is a nice combo</h1>);
+// Another DOM element injected
+[...document.getElementsByClassName('containerElement')].forEach(el => {
+    ReactDOM.createRoot(el!).render(<h1 className="text-2xl">Hello world! this is a nice combo</h1>);
+});
